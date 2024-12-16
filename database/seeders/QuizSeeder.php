31f3e86adc38d4ba740f55526358289616608e35
Misia Abdipatra <misiabdipatra@gmail.com>
2024-12-16 13:52:08 +0700

    update

--- database/seeders/QuizSeeder.php
+++ database/seeders/QuizSeeder.php
@@ -8,8 +8,17 @@ use Illuminate\Support\Facades\DB;
 class QuizSeeder extends Seeder
 {
     public function run()
-    {   
-        // pertanyaan untuk quiz
+    {
+        // Tambahkan quiz
+        $quizId = DB::table('quizzes')->insertGetId([
+            'course_id' => 21, // ID course terkait
+            'title' => 'Exploring the Wonders of Science',
+            'description' => 'Test your basic science knowledge.',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        
+        // Tambahkan pertanyaan untuk quiz
         DB::table('questions')->insert([
             [
                 'quiz_id' => $quizId,
